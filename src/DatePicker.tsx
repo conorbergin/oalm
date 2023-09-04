41510f032e51f3c3e070a9e35c5965daabb78295
conor <1299651405@mail.com>
2023-09-04 04:09:55 +0100

    date imporvements

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -84,38 +84,36 @@ const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 
 const TASKEVENT = '10'
 
-export const MaybeDT: Component<{ node: Y.Map<any> }> = (props) => {
-  const [hasDT, setHasDT] = createSignal(props.node.has(TASKEVENT))
+// export const MaybeDT: Component<{ node: Y.Map<any> }> = (props) => {
+//   const [hasDT, setHasDT] = createSignal(props.node.has(TASKEVENT))
 
-  const f = () => setHasDT(props.node.has(TASKEVENT))
-  props.node.observe(f)
-  onCleanup(() => props.node.unobserve(f))
-  return (
-    <Show when={hasDT()} fallback={
-      <button class="text-gray-300" onClick={() => {
-        props.node.set(TASKEVENT, { begin: { kind: 'none' }, end: { kind: 'none' } })
-      }}>set task/event</button>
-    }>
-      <ErrorBoundary fallback={<span class='text-red-600'>Error</span>}>
-        <DTNode node={props.node} />
-      </ErrorBoundary>
-    </Show>
-  )
-}
+//   const f = () => setHasDT(props.node.has(TASKEVENT))
+//   props.node.observe(f)
+//   onCleanup(() => props.node.unobserve(f))
+//   return (
+//     <Show when={hasDT()} fallback={
+//       <button class="text-gray-300" onClick={() => }>set task/event</button>
+//     }>
+//       <ErrorBoundary fallback={<span class='text-red-600'>Error</span>}>
+//         <DTNode node={props.node} />
+//       </ErrorBoundary>
+//     </Show>
+//   )
+// }
 
 
 const taskEventString = (t: TaskEvent) => {
-  switch (t.type) {
-    case DATEDATE: {
-      const b = Temporal.PlainDate.from(t.begin!)
-      const e = Temporal.PlainDate.from(t.end!)
-      if (b.year !== e.year) return b.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' }) + ' - ' + e.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
-      if (b.month !== e.month) return b.toLocaleString('en-GB', { day: 'numeric', month: 'short' }) + ' - ' + e.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
-      if (b.day !== e.day) return b.toLocaleString('en-GB', { day: 'numeric' }) + ' - ' + e.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
-      return 'Error: Dates out of order'
-    }
-    default: return 'Default'
+  if (t.begin && t.end) {
+    const b = Temporal.PlainDate.from(t.begin!)
+    const e = Temporal.PlainDate.from(t.end!)
+    if (b.year !== e.year) return b.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' }) + ' - ' + e.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
+    if (b.month !== e.month) return b.toLocaleString('en-GB', { day: 'numeric', month: 'short' }) + ' - ' + e.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
+    if (b.day !== e.day) return b.toLocaleString('en-GB', { day: 'numeric' }) + ' - ' + e.toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
+    return 'Invalid date interval'
   }
+  if (t.end) return Temporal.PlainDate.from(t.end).toLocaleString('en-GB', { day: 'numeric', month: 'short', year: '2-digit' })
+  if (t.duration) return Temporal.Duration.from(t.duration).toLocaleString('en-GB')
+  return 'err'
 }
 
 const durationOptions = ['1w', '2w', '3w', '1m', '2m', '3m', '6m']
@@ -128,21 +126,25 @@ const TASK = 't'
 
 
 
-export const DTNode: Component<{ node: Y.Map<any> }> = (props) => {
+export const MaybeDT : Component<{ node: Y.Map<any> }> = (props) => {
 
 
   const date = ySignal(props.node, TASKEVENT)
+  console.log(date())
+  createEffect(() => {
+    console.log(date())
+  })
 
 
 
   let r
   return (
     <>
-      <button contentEditable={false} class="text-green-700 " classList={{
-        'line-through text-gray-500': date().done,
+      <button contentEditable={false} class="text-gray-400 " classList={{
+        'text-green-700': date(),
       }} onClick={() => r.showModal()}>
-        {taskEventString(date())}
-        <Show when={!date().done}>
+        <Show when={date()} fallback={'set task/event'} >{taskEventString(date())}</Show>
+        <Show when={false}>
           <span class="text-gray-500">
             <DepTracker node={props.node} />
           </span>
@@ -157,55 +159,30 @@ export const DTNode: Component<{ node: Y.Map<any> }> = (props) => {
   )
 }
 
-const TaskSelector: Component<{ date: any }> = (props) => {
-  return (
-    <>
-      <div class='flex gap-1'>
-        <div>Due:</div>
-        <For each={vagueOptions}>
-          {(item) => <button onClick={() => { }}>{item}</button>}
-        </For>
-        <input type='date'></input>
-      </div>
-      <div class='flex gap-1'>
-        <div>
-          Duration:
-        </div>
-        <For each={durationOptions}>
-          {(item) => <button>{item}</button>}
-        </For>
-      </div>
-    </>
-  )
-}
 
 export const DateSelector: Component<{ date: any, node: Y.Map<any> }> = (props) => {
+  const [screen, setScreen] = createSignal(0)
 
   return (
-    <div class='flex-col gap-1 p-2'>
-      <div class='flex gap-2'>
-        <button onClick={() => props.node.set(TASKEVENT, { type: DATEDATE })}>date/date</button>
-        <button onClick={() => props.node.set(TASKEVENT, { type: TASK })}>task</button>
+    <div class='flex-col gap-1 p-2 '>
+      <div class='flex gap-2 justify-center'>
+        <button classList={{ 'text-gray-500': screen() !== 0 }} onClick={() => setScreen(0)}>event</button>
+        <button classList={{ 'text-gray-500': screen() !== 1 }} onClick={() => setScreen(1)}>task</button>
+        <button classList={{ 'text-gray-500': screen() !== 1 }} onClick={() => props.node.delete(TASKEVENT)}>clear</button>
       </div>
       <Switch>
-        <Match when={props.date.type === TASK}>
-          <div>
-            Duration:
-            <select>
-
-            </select>
-          </div>
-
-        </Match>
-        <Match when={props.date.type === 'date'}>
-          <div>
-            <div><input type='date' value={props.date.end} onChange={(e) => props.node.set(TASKEVENT, { type: props.date.type, begin: props.date.begin, end: e.target.value })} /> </div>
+        <Match when={screen() === 0}>
+          <div class='grid gap-1 items-center' style='grid-template-columns: 5rem 12rem 5rem'>
+            <div class='text-end'>Begin: </div>
+            <input disabled={!props.date?.end} max={(props.date?.end) ? props.date.end : ''} type='date'  value={(props.date?.begin) ? props.date.begin : ''} onChange={(e) => props.node.set(TASKEVENT, { begin: e.target.value, end: props.date.end })} />
+            <button onClick={() => props.node.set(TASKEVENT, { end: props.date.end })}>clear</button>
+            <div class='text-end'>End: </div>
+            <input type='date' value={(props.date?.end) ? props.date.end : ''} min={(props.date?.begin) ? props.date.begin : ''} onChange={(e) => props.node.set(TASKEVENT, { ...(props.date && props.date.begin && { begin: props.date.begin }), end: e.target.value })} />
           </div>
         </Match>
-        <Match when={props.date.type === DATEDATE}>
+        <Match when={screen() === 1}>
           <div>
-            <div>Begin: <input type='date' value={props.date.begin} onChange={(e) => props.node.set(TASKEVENT, { type: props.date.type, begin: e.target.value, end: props.date.end })} /></div>
-            <div>End: <input type='date' value={props.date.end} onChange={(e) => props.node.set(TASKEVENT, { type: props.date.type, begin: props.date.begin, end: e.target.value })} /></div>
+            Duration: TODO
           </div>
         </Match>
       </Switch>
